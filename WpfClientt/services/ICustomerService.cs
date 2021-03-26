19260bca36a2b15c1d1cbfdc34ab0133c5e572b8
Barracuda <dimitrijchannel@gmail.com>
2021-03-26 17:42:08 +0200

    Changes: 1)Added main service interfaces(IScroller,IPage,IService,IAdService,ICustomerService). 2)Added implementation of IScroller ( GenericScroller) and IPage( GenericPage). 3)Added implementations of IAdService(AdServiceImpl) and ICustomerService(CustomerServiceImpl).Not yet implemented.

---
+++ WpfClientt/services/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClientt.services {
+    public interface ICustomerService : IService<Customer>{
+
+        bool Login(string username, string password);
+
+    }
+}
---
+++ WpfClientt/services/IScroller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClientt.services {
+    public interface IScroller<T>  {
+
+        /// <summary>
+        /// Initializes the scroller.Must be called before use!
+        /// Must not call any other method of this class until Init has completed.
+        /// </summary>
+        Task Init();
+
+        /// <summary>
+        /// Returns the current page.
+        /// </summary>
+        /// <returns></returns>
+        IPage<T> CurrentPage();
+
+        /// <summary>
+        /// Moves to the next page,if possible.
+        /// After this method is completed,to retrieve the new page,call CurrentPage again.
+        /// </summary>
+        /// <returns>Returns true if next page retrieved,false otherwise.</returns>
+        Task<bool> MoveNext();
+
+        /// <summary>
+        /// Moves to the previous page,if possible.
+        /// After this method is completed,to retrieve the new page,call CurrentPage again.
+        /// </summary>
+        /// <returns>Returns true if previous page retrieved,false otherwise.</returns>
+        Task<bool> MoveBack();
+
+        int NumberOfPages();
+
+    }
+}
