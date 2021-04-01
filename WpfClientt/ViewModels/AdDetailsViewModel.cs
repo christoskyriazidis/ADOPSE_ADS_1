e12007cca1c7295d6dc0676da4cf38b0a64d2286
Barracuda <dimitrijchannel@gmail.com>
2021-04-01 09:25:33 +0300

    Changes: 1)Added AdDetailsViewModel that has one property of type Ad. 2)Replaced specific hexes of colors with static resources in AdDetails 3)Added binding to Read more buttons in AdsView to ReadMoreCommand of AdsViewModel

---
+++ WpfClientt/ViewModels/AdDetailsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfClientt.model;
+using WpfClientt.services;
+
+namespace WpfClientt.viewModels {
+    public class AdDetailsViewModel : BaseViewModel,IViewModel {
+
+        private IAdService adService;
+        private Ad ad;
+
+        public Ad DisplayedAd {
+            get {
+                return ad;
+            }
+            private set {
+                ad = value;
+                OnPropertyChanged("DisplayedAd");
+            } 
+        }
+
+        public AdDetailsViewModel(FactoryServices factory, long id) {
+            adService = factory.AdServiceInstance();
+            DisplayedAd = adService.ReadById(id).Result;
+        }
+
+    }
+}
