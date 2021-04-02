df9732415bfecba5e9ef14ff59b2730e58d95531
christoskyriazidis <52704116+christoskyriazidis@users.noreply.github.com>
2021-04-02 10:30:17 +0300

    big hacks

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -23,7 +23,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +49,7 @@ namespace WebApplication1
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
