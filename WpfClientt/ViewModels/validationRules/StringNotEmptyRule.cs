80630ca9b2f8fd123bd36fe1a3cd72f649b7efdf
Barracuda <dimitrijchannel@gmail.com>
2021-04-17 15:54:11 +0300

    Changes: 1)Added validation rules(not yet used).

---
+++ WpfClientt/ViewModels/validationRules/StringNotEmptyRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfClientt.viewModels.validationRules {
+    public abstract class StringNotEmptyRule : ValidationRule {
+
+        private string field;
+
+        public StringNotEmptyRule(string field) {
+            this.field = field;
+        }
+
+
+        public sealed override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            if(value == null || !(value is string) || ((string)value).Length == 0) {
+                return new ValidationResult(false, $"Provide value for the {field}");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+}
