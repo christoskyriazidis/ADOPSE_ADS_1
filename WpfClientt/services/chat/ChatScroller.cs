0109227adc7e31f56f46a481777cb134208e7656
Barracuda <dimitrijchannel@gmail.com>
2021-05-06 15:09:31 +0300

    Changes: 1)Added datatemplate mappings into App.xaml for Chat and ChatViewModel classes.Also added converter that reverses the boolean value(needed for binding). 2)Added ChatViewModel that displays a single chat. 3)ChatsViewModel changed: 	a)No longer has Messages,SendCommand,Message properties - those moved to ChatViewModel. 	b)Added SelectedChat property that points to the selected chat and changed when another is selected.For tha later,now have 	the SelectChatCommand that changes the selected chat and loads its messages. 4)In LoginCustomerMenu,all delegate commands are placed with async commands,and the notify is awaited.Also the notification does not show counter anymore, but just a message(new) near the Notifications. 5)Chat model class changed:removed most proeprties.Now the only properties are:Customer,Ad,Sold,ChatId 6)In Message,added json property mappings. 7)Added ChatModel class that is used to load the returned chat information from the server. 8)Fixed ChatScroller:now properly using the chat to build the url and next number is not increamented as before(which was a mistake,since it was getting increamented in Init and skipping one page). 9)In ChatServiceSignalR: 	a)Added active chat listeners that get a new chat when it comes out.Implemented the logic that calls the listeners if the incoming chat 	is related to the logged in customer. 	b)Added private method that returns chats in a form of ChatModel instances retrieved from the server. 	c)Implemented SendMessage(not yet tested). 	d)Chats now uses the new private method and then maps each ChatModel instance into Chat instnaces. 	e)Scrolelr is implemented. 10)In IChatService: 	a)added methods for adding active chat listeners. 	b)Messages now accepts instance of the class Chat,instead of just an id. 11)Added ChatBoxView.xaml view that is used to display the chat instnaces as buttons the user can click. 12)Changed in ChatView.xaml: 	a)added items control that hsows messages. 	b)added textbox area for specifying the message 	c)added send button.This button uses the new ReverseBooleanVaueConverter. 13)In ChatsView.xaml each chat is shown as a button,whose content is specified by the datatemplate in App.xaml.Removed messages(moved into ChatView). 14)Smalls changes in MessageView.

--- WpfClientt/services/chat/ChatScroller.cs
+++ WpfClientt/services/chat/ChatScroller.cs
@@ -10,14 +10,14 @@ using WpfClientt.model;
 namespace WpfClientt.services {
     class ChatScroller : IScroller<Message> {
         private object lockObject = new object();
-        private int chatId;
+        private Chat chat;
         private HttpClient client;
         private int nextPageNumber = 1;
         private ChatPage currentPage;
 
-        public ChatScroller(int chatId,HttpClient client) {
+        public ChatScroller(Chat chat,HttpClient client) {
             this.client = client;
-            this.chatId = chatId;
+            this.chat = chat;
         }
 
         public async Task Init() {
@@ -40,17 +40,17 @@ namespace WpfClientt.services {
                 nextPageNumber = nextPageNumber - 1;
             }
             await RetrievePage(pageNumber);
-            return true;
+            return CurrentPage().Objects().Count == 0;
         }
 
         public async Task<bool> MoveNext() {
             int pageNumber;
             lock (lockObject) {
-                pageNumber = nextPageNumber+1;
+                pageNumber = nextPageNumber;
                 nextPageNumber = nextPageNumber + 1;
             }
             await RetrievePage(pageNumber);
-            return true;
+            return CurrentPage().Objects().Count != 0;
         }
 
         public int NumberOfPages() {
@@ -59,7 +59,7 @@ namespace WpfClientt.services {
         }
 
         private async Task RetrievePage(int pageNumber) {
-            string url = ApiInfo.MessageMainUrl() + "/" + pageNumber;
+            string url = $"{ApiInfo.MessageMainUrl()}?PageNumber={pageNumber}&ChatId={chat.ChatId}";
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
             using (HttpResponseMessage response = await client.SendAsync(request)) {
                 response.EnsureSuccessStatusCode();
